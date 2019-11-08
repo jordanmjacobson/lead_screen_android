fd0890208323726fe1cb8500a983c63202c1d3a0
Jordan Jacobson <jjacob44@binghamton.edu>
2019-11-08 16:27:42 -0500

    added files

---
+++ app/src/main/java/com/example/leadscreenguide/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.leadscreenguide;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
