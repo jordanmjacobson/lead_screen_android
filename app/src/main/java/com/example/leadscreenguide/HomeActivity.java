1b47260140a4531f9492127c12190b60c274794f
Lesly Canales <leslyc977@gmail.com>
2019-11-10 22:37:16 -0500

    added capillary/venous setup

--- app/src/main/java/com/example/leadscreenguide/HomeActivity.java
+++ app/src/main/java/com/example/leadscreenguide/HomeActivity.java
@@ -22,6 +22,18 @@ public class HomeActivity extends AppCompatActivity {
 
     private Button button2;
 
+    private Button button3;
+
+    private Button button4;
+
+    private Button button5;
+
+    private Button button6;
+
+    private Button button7;
+
+    private Button button8;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +50,22 @@ public class HomeActivity extends AppCompatActivity {
                     doSurvey();
                 }
 
-                if (choice== 2) {
+                else if (choice== 2) {
                     //open capillary test
+                    setChoice(7);
+                    doTest();
+                }
+                else if (choice ==7) {
+                    //do less than 5 ug for capillary test
+                    undoTest();
+                    holder1.setText("do less than 5 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do less than 5 ug for venous test
+                    undoTest();
+                    holder1.setText("do less than 5 ug for venous test\n");
 
                 }
-
                 //if any other choices do nothing
 
             }
@@ -52,6 +75,114 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (choice == 2) {
                     //open venous test
+                    setChoice(8);
+                    doTest();
+                }
+                else if (choice ==7) {
+                    //do 5-9 ug for capillary test
+                    undoTest();
+                    holder1.setText("do 5-9 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do 5-9 ug for venous test
+                    undoTest();
+                    holder1.setText("do 5-9 ug for venous test\n");
+                }
+                //if any other choices do nothing
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choice ==7) {
+                    //do 10-14 ug for capillary test
+                    undoTest();
+                    holder1.setText("do 10-14 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do 10-14 ug for venous test
+                    undoTest();
+                    holder1.setText("do 10-14 ug for venous test\n");
+                }
+                //if any other choices do nothing
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choice ==7) {
+                    //do 15-24 ug for capillary test
+                    undoTest();
+                    holder1.setText("do 15-24 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do 15-24 ug for venous test
+                    undoTest();
+                    holder1.setText("do 15-24 ug for venous test\n");
+                }
+                //if any other choices do nothing
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choice ==7) {
+                    //do 25-44 ug for capillary test
+                    undoTest();
+                    holder1.setText("do 25-44 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do 25-44 ug for venous test
+                    undoTest();
+                    holder1.setText("do 25-44 ug for venous test\n");
+                }
+                //if any other choices do nothing
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choice ==7) {
+                    //do 45-59 ug for capillary test
+                    undoTest();
+                    holder1.setText("do 45-59 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do 45-59 ug for venous test
+                    undoTest();
+                    holder1.setText("do 45-59 ug for venous test\n");
+                }
+                //if any other choices do nothing
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choice ==7) {
+                    //do 60-69 ug for capillary test
+                    undoTest();
+                    holder1.setText("do 60-69 ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do 60-69 ug for venous test
+                    undoTest();
+                    holder1.setText("do 60-69 ug for venous test\n");
+                }
+                //if any other choices do nothing
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (choice ==7) {
+                    //do >= 70 ug for capillary test
+                    undoTest();
+                    holder1.setText("do >=70ug for capillary test\n");
+                }
+                else if (choice ==8) {
+                    //do >= 70 ug for venous test
+                    undoTest();
+                    holder1.setText("do >=70ug for venous test\n");
                 }
                 //if any other choices do nothing
             }
@@ -71,6 +202,24 @@ public class HomeActivity extends AppCompatActivity {
         }
         if (button2==null){
             button2= findViewById(R.id.button2);
+        }
+        if (button3==null){
+            button3= findViewById(R.id.button3);
+        }
+        if (button4==null){
+            button4= findViewById(R.id.button4);
+        }
+        if (button5==null){
+            button5= findViewById(R.id.button5);
+        }
+        if (button6==null){
+            button6= findViewById(R.id.button6);
+        }
+        if (button7==null){
+            button7= findViewById(R.id.button7);
+        }
+        if (button8==null){
+            button8= findViewById(R.id.button8);
         }
     }
 
@@ -158,8 +307,49 @@ public class HomeActivity extends AppCompatActivity {
 
         holder1.setText("survey stuff would go here");
     }
+
+    public void doTest() {
+        if (choice==7) {
+            getSupportActionBar().setTitle("Capillary Sample");
+        }
+        else if (choice==8) {
+            getSupportActionBar().setTitle("Venous Sample");
+        }
+        holder1.setText("Select the lead level result(ug/dL): ");
+        holder2.setVisibility(View.GONE);
+        button3.setVisibility(View.VISIBLE);
+        button4.setVisibility(View.VISIBLE);
+        button5.setVisibility(View.VISIBLE);
+        button6.setVisibility(View.VISIBLE);
+        button7.setVisibility(View.VISIBLE);
+        button8.setVisibility(View.VISIBLE);
+
+        button1.setText("<5 ug/dL");
+        button2.setText("5-9");
+        button3.setText("10-14");
+        button4.setText("15-24");
+        button5.setText("25-44");
+        button6.setText("45-59");
+        button7.setText("60-69");
+        button8.setText(">= 70");
+
+    }
+
+    public void undoTest() {
+        button1.setVisibility(View.GONE);
+        button2.setVisibility(View.GONE);
+        button3.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
+        button5.setVisibility(View.GONE);
+        button6.setVisibility(View.GONE);
+        button7.setVisibility(View.GONE);
+        button8.setVisibility(View.GONE);
+    }
+
     public static void setChoice(int num) {
         choice= num;
     }
+
+
 
 }
